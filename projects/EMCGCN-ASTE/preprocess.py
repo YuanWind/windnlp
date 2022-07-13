dc727686edee7726fa0e1c3712d9ca719286f13b
wind <yywind@126.com>
2022-07-13 20:26:12 +0800

    项目EMCGCN

---
+++ projects/EMCGCN-ASTE/preprocess.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+'''
+@File    :   preprocess.py
+@Time    :   2022/07/12 15:57:52
+@Author  :   Yuan Wind
+@Desc    :   None
+'''
+import sys
+sys.path.extend(['../../','./','../'])
+import logging
+from modules.data.vocabs import ASTEVocab
+from scripts.utils import dump_pkl
+logger = logging.getLogger(__name__.replace('_', ''))
+
+
+def build_vocab(train_files, dev_files, test_files, vocab_file):
+    vocab = ASTEVocab()
+    vocab.read_files(train_files, 'train')
+    vocab.read_files(dev_files, 'dev')
+    vocab.read_files(test_files, 'test')
+    vocab.build()
+    dump_pkl(vocab, vocab_file)
+    print(f'Save vocab to {vocab_file}')
+    
+    
+
+
+if __name__ == '__main__':
+    train_files = ['projects/EMCGCN-ASTE/data/sample.json']
+    dev_files = ['projects/EMCGCN-ASTE/data/sample.json']
+    test_files = ['projects/EMCGCN-ASTE/data/sample.json']
+    vocab_file = 'projects/EMCGCN-ASTE/data/vocab.pkl'
+    build_vocab(train_files, dev_files, test_files, vocab_file)
